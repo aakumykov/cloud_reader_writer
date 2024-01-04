9984e5f45e4631988005713f8f622737c5165ded
Andrey Kumykov <aakumykov@yandex.ru>
2024-01-04 23:56:53 +0300

    Удалил неиспользуемый код из BasicCloudWriter.

--- cloud_writer/src/main/java/com/github/aakumykov/cloud_writer/BasicCloudWriter.kt
+++ cloud_writer/src/main/java/com/github/aakumykov/cloud_writer/BasicCloudWriter.kt
@@ -2,28 +2,8 @@ package com.github.aakumykov.cloud_writer
 
 abstract class BasicCloudWriter : CloudWriter {
 
-    /*fun iterateThroughDirHierarchy(dirName: String, onNextDirName: (nextDirName: String) -> Unit) {
-        with(dirName) {
-            this.split(DS).reduce { nextDirPath, nextDirName ->
-                val dirPath = "$nextDirPath/$nextDirName"
-                onNextDirName(nextDirPath)
-                dirPath
-            }
-        }
-    }*/
-
-    private fun restoreStartingSlash(dirName: String): String {
-        return (DS + dirName).replace(Regex("^[${DS}]+"), DS)
-    }
-
-
     protected fun composePath(parentDirName: String, childDirName: String): String
             = "${parentDirName}${CloudWriter.DS}${childDirName}".stripMultiSlash()
-
-
-    companion object {
-        private const val DS = "/"
-    }
 }
 
 private fun String.stripMultiSlash(): String {
