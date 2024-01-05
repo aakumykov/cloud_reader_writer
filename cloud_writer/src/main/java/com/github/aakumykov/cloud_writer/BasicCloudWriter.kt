1e29704c4eaf114adcf0bdb1a25673f289167348
Andrey Kumykov <aakumykov@yandex.ru>
2024-01-05 13:46:28 +0300

    TODO: избавиться от этого базового класса в BasicCloudWriter.

--- cloud_writer/src/main/java/com/github/aakumykov/cloud_writer/BasicCloudWriter.kt
+++ cloud_writer/src/main/java/com/github/aakumykov/cloud_writer/BasicCloudWriter.kt
@@ -2,6 +2,7 @@ package com.github.aakumykov.cloud_writer
 
 abstract class BasicCloudWriter : CloudWriter {
 
+    // TODO: избавиться от этого базового класса
     protected fun composePath(parentDirName: String, childDirName: String): String
             = "${parentDirName}${CloudWriter.DS}${childDirName}".stripMultiSlash()
 }
