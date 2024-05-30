a97b5cd6325b27520a287b533d31443e04c14526
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-30 13:01:02 +0300

    Упростил, всё равно не хочет.

--- app/src/main/java/com/github/aakumykov/cloud_reader_writer/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/cloud_reader_writer/MainActivity.kt
@@ -2,38 +2,18 @@ package com.github.aakumykov.cloud_reader_writer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.github.aakumykov.cloud_reader_writer.databinding.ActivityMain2Binding
 import com.github.aakumykov.cloud_reader_writer.fragments.Fragment1
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMain2Binding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main2)
 
-        /*enableEdgeToEdge()
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.rootView) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
-
-        /*supportFragmentManager
+        supportFragmentManager
             .beginTransaction()
-            .add(R.id.fragmentContainerView, BlankFragment(), null)
-            .commit()*/
-
-        /*supportFragmentManager
-            .beginTransaction()
-            .add(R.id.fragmentContainerView2, Fragment1(), null)
-            .commit()*/
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainerView2, Fragment1())
+            .replace(R.id.fragmentContainerView, Fragment1())
             .commit()
     }
 }
