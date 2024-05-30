f370f37f6edb064e01bc959bcb0cfdc6df36a218
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-30 12:49:19 +0300

    Фрагмент не хочет работать.

---
+++ app/src/main/java/com/github/aakumykov/cloud_reader_writer/fragments/Fragment1.kt
@@ -0,0 +1,7 @@
+package com.github.aakumykov.cloud_reader_writer.fragments
+
+import androidx.fragment.app.Fragment
+import com.github.aakumykov.cloud_reader_writer.R
+
+class Fragment1 : Fragment(R.layout.fragment1) {
+}
