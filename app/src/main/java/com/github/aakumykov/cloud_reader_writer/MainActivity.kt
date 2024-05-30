fa3b7dea41345533013f2c52ed9e8d57a82b42ac
Andrey Kumykov <aakumykov@yandex.ru>
2024-05-30 13:13:34 +0300

    Наконец, заработало.

--- app/src/main/java/com/github/aakumykov/cloud_reader_writer/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/cloud_reader_writer/MainActivity.kt
@@ -2,22 +2,22 @@ package com.github.aakumykov.cloud_reader_writer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.github.aakumykov.cloud_reader_writer.databinding.ActivityMain2Binding
-import com.github.aakumykov.cloud_reader_writer.fragments.Fragment1
+import com.github.aakumykov.cloud_reader_writer.databinding.ActivityMainBinding
+import com.github.aakumykov.cloud_reader_writer.fragments.ReadingAndDirCreationFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMain2Binding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMain2Binding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.fragmentContainerView2, Fragment1())
+            .replace(R.id.fragmentContainerView, ReadingAndDirCreationFragment.create())
             .commit()
     }
 }
--- app/src/main/java/com/github/aakumykov/cloud_reader_writer/fragments/Fragment1.kt
+++
@@ -1,7 +0,0 @@
-package com.github.aakumykov.cloud_reader_writer.fragments
-
-import androidx.fragment.app.Fragment
-import com.github.aakumykov.cloud_reader_writer.R
-
-class Fragment1 : Fragment(R.layout.fragment1) {
-}
